b87b3b94c3cc239ca73d333250d24afd9b4bfc81
Dinuka Ekanayake <dinukaekanayake92@gmail.com>
2024-05-19 12:05:45 +0800

    first commit

---
+++ src/main/java/com/poc/springboot/kubernetes/demo/SpringbootKubernetesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.poc.springboot.kubernetes.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKubernetesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKubernetesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/springboot/kubernetes/demo/SpringbootKubernetesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.springboot.kubernetes.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKubernetesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
